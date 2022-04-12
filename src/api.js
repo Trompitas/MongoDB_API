89415892cbc80c3a9dc89b8159fb085661c7b2ee
Trompitas <90296185+Trompitas@users.noreply.github.com>
2022-04-11 22:42:38 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 // settings
 const app = express();
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 // middlewares
