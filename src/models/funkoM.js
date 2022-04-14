79e8818cdf33df2a1f2fdffe18482612b6e23a2e
Trompitas <90296185+Trompitas@users.noreply.github.com>
2022-04-13 20:07:08 -0500

    Update funkoM.js

--- src/models/funkoM.js
+++ src/models/funkoM.js
@@ -3,27 +3,39 @@ const mongoose = require("mongoose");
 const funkoSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    versionKey: false,
+    timestamps: true,
   },
   price: {
     type: Number,
-    requiered: true
+    requiered: true,
+    versionKey: false,
+    timestamps: true,
   },
   material:{
     type: String,
-    required: true
+    required: true,
+    versionKey: false,
+    timestamps: true,
   },
   stock: {
     type: Number,
-    required: true
+    required: true,
+    versionKey: false,
+    timestamps: true,
   },
   coleccion: {
     type: String,
-    required: true
+    required: true,
+    versionKey: false,
+    timestamps: true,
   },
   funkoImage:{
     type: String,
-    required: true
+    required: true,
+    versionKey: false,
+    timestamps: true,
   }
 });
 
